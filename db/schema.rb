07f15bf0d4963e923b849bfa7a48b1b79523b111
danthetreeman <dchuckrey.inc@gmail.com>
2016-11-30 20:31:12 -0600

    Before Category Search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128221059) do
+ActiveRecord::Schema.define(version: 20161128205005) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "namespace"
@@ -128,12 +128,10 @@ ActiveRecord::Schema.define(version: 20161128221059) do
     t.string   "address"
     t.integer  "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["province_id"], name: "index_users_on_province_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "trees"
   add_foreign_key "orders", "order_statuses"
-  add_foreign_key "users", "provinces"
 end
